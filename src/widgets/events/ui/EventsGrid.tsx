831703912f00c9477195320250e7eec9af2e909a
김종현 <jhk9027@gmail.com>
2026-02-27 22:42:58 +0900

    update: meetup #2

--- src/widgets/events/ui/EventsGrid.tsx
+++ src/widgets/events/ui/EventsGrid.tsx
@@ -58,8 +58,8 @@ const events: Event[] = [
     id: 'mug-korea-seoul-2026-1',
     category: 'meetup',
     type: 'inPerson',
-    status: 'open',
-    statusText: EventDescription.open,
+    status: 'closed',
+    statusText: EventDescription.closed,
     time: '19:00 ~ 21:30',
     description: `- 2026 MongoDB 최신 기술 동향
     - 인덱스 & 쿼리 최적화 실전 지식 (Community Pick!)
@@ -69,11 +69,11 @@ const events: Event[] = [
     FEB 2026`,
     date: '2026-02-26',
     location: '서울 강남구 역삼동 629-1 인사이트 빌딩 지하1층',
-    attendees: '00(모집 중)',
+    attendees: '28',
     action: 'MeetUp 페이지 바로가기',
     link: 'https://www.meetup.com/mongodb-usergroup-seoul/events/313060611/?eventOrigin=notifications&notificationId=%3Cinbox%3E%21477271970-1770083887463',
     createdAt: '2026-01-15',
-    updatedAt: '2026-02-13',
+    updatedAt: '2026-02-27',
   },
   {
     id: 'ai-skills-session-3',
